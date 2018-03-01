e13fe2abed8b46f2e8b1cdfa242f597232981cba
rosbertguevarra <crisiscorefair@gmail.com>
2018-03-01 13:32:28 +0800

    updated apps

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const db = require('./db');
 
+const UserController = require('./user/UserController');
+app.use("/users", UserController)
+
 
 module.exports = app;
 
