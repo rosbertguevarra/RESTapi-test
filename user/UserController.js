1632f16c7fa1c88024f190db07cc1f21666c021b
rosbertguevarra <crisiscorefair@gmail.com>
2018-03-01 13:06:47 +0800

    updated the UserController

--- user/UserController.js
+++ user/UserController.js
@@ -5,3 +5,24 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({extended: true}))
 router.use(bodyParser.json());
+
+
+const User = require('./User');
+
+router.post('/', (req,res)=>{
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    },
+    (err,user)=>{
+        if(err){
+            return res.status(500).send("there was a problem finding the user");
+            res.status(200).send(users);
+        }
+    }
+)
+})
+
+
+module.exports = router;
